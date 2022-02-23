64d662202d0678ec18294636ba255a9ea4cb9c1f
EmmaHammar <emma.s.hammar@gmail.com>
2022-02-23 14:09:50 +0100

    add step icons

--- src/components/home/main-section3.js
+++ src/components/home/main-section3.js
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import gsap from 'gsap';
+
 import styles from './main-section3.module.scss'; 
+import StepIcon1 from './step-icon1';
+import StepIcon2 from './step-icon2';
+import StepIcon3 from './step-icon3';
+
+
 
 export default function MainSection3() {
+  
+  useEffect( () => {
+  
+    // gsap.to('#step1', {
+    //   //viewBox="0 0 738 182.767"> //default
+    //   attr: {viewBox:'0 0 500 -50', width:'220'}, 
+    // });
+      
+  
+  }, []);
+
+  
+
   return (
     <>
       <div id='stepTextWrapper' className={styles.stepTextWrapper}>
         <h3 className={styles.h3}>Så funkar det</h3>
-        <p>Följ dessa tre steg för att komma igång. Tveka inte att höra av dig till oss om du får frågor längs vägen - vi stöttar dig i hamn!</p>
+        <p>Tre enkla steg för att skapa din brandade kupong. Vårt fantastiska team guidar dig längs vägen!</p>
       </div>
       
       <div id='stepCardsWrapper'className={styles.stepCardsWrapper} >
         <div id='stepCard1' className={styles.stepCard}>
-          <div className='stepIconsWrapper'>
-            <div className='stepNumber'>Stepnumber</div>
-            <div className='stepIcon'>Stepicon</div>
+          <StepIcon1/>
+          <div className={styles.stepText}>
+            <h4 className={styles.h4}>1. Logga in</h4>
+            <p>Logga in i vårt verktyg! Skicka ut obegränsat med kuponger för 599 kr/mån.</p>
           </div>
-          <h5>Logga in</h5>
-          <p>Logga in i vårt verktyg! Skicka ut obegränsat med kuponger för 599 kr/mån.</p>
         </div>
+
         <div id='stepCard2' className={styles.stepCard}>
-          <div className='stepIconsWrapper'>
-            <div className='stepNumber'>Stepnumber</div>
-            <div className='stepIcon'>Stepicon</div>
+          <StepIcon2/>
+          <div className={styles.stepText}>
+            <h4 className={styles.h4}>2. Branda kupongen</h4>
+            <p>Välj hur du vill att kupongutskicket ska se ut. Designa fritt eller få stöd från våra mallar. </p>
           </div>
-          <h5>Branda kupongen</h5>
-          <p>Välj hur du vill att kupongutskicket ska se ut. Designa fritt eller få stöd från våra mallar. </p>
         </div>
         <div id='stepCard3' className={styles.stepCard}>
-          <div className='stepIconsWrapper'>
-            <div className='stepNumber'>Stepnumber</div>
-            <div className='stepIcon'>Stepicon</div>
+          <StepIcon3/>
+          <div className={styles.stepText}>
+            <h4 className={styles.h4}>3. Fördjupa dina målgruppsinsikter</h4>
+            <p>Följ statistik över vilka som använder kupongen. Skapa nya utskickslistor baserat på tidigare beteenden.</p>
           </div>
-          <h5>Fördjupa dina insikter om din målgrupp</h5>
-          <p>Följ statistik över vilka som använder kupongen. Skapa nya utskickslistor baserat på tidigare beteenden.</p>
         </div>
         
       </div>
---
+++ src/components/home/step-icon2.js
@@ -0,0 +1,120 @@
+import React from 'react';
+
+export default function StepIcon2() {
+  return (
+    <div>
+        <svg id="stepIcon2" xmlns="http://www.w3.org/2000/svg" width="226.5" height="182.767" viewBox="44 -16 226.5 182.767">
+            <g id="qrcodeicon" transform="translate(97.6 18.854)">
+                <g id="qrhandicon" transform="translate(-75.1 -19.999)">
+                <g id="qrcode-icon" transform="translate(22.5 46.202)">
+                    <g id="Rectangle_65" data-name="Rectangle 65" transform="translate(2.734 1.28)" fill="none" stroke="#34342c" strokeWidth="3">
+                    <rect width="37.366" height="37.366" stroke="none"/>
+                    <rect x="1.5" y="1.5" width="34.366" height="34.366" fill="none"/>
+                    </g>
+                    <g id="Rectangle_68" data-name="Rectangle 68" transform="translate(58.784 1.28)" fill="none" stroke="#34342c" strokeWidth="3">
+                    <rect width="37.366" height="37.366" stroke="none"/>
+                    <rect x="1.5" y="1.5" width="34.366" height="34.366" fill="none"/>
+                    </g>
+                    <g id="Rectangle_70" data-name="Rectangle 70" transform="translate(58.784 57.329)" fill="none" stroke="#34342c" strokeWidth="3">
+                    <rect width="37.366" height="37.366" stroke="none"/>
+                    <rect x="1.5" y="1.5" width="34.366" height="34.366" fill="none"/>
+                    </g>
+                    <g id="Rectangle_72" data-name="Rectangle 72" transform="translate(1.4 57.329)" fill="none" stroke="#34342c" strokeWidth="3">
+                    <rect width="37.366" height="37.366" stroke="none"/>
+                    <rect x="1.5" y="1.5" width="34.366" height="34.366" fill="none"/>
+                    </g>
+                    <g id="Rectangle_66" data-name="Rectangle 66" transform="translate(14.745 13.29)" fill="none" stroke="#34342c" strokeWidth="3">
+                    <rect width="16.014" height="16.014" stroke="none"/>
+                    <rect x="1.5" y="1.5" width="13.014" height="13.014" fill="none"/>
+                    </g>
+                    <g id="Rectangle_67" data-name="Rectangle 67" transform="translate(70.794 13.29)" fill="none" stroke="#34342c" strokeWidth="3">
+                    <rect width="16.014" height="16.014" stroke="none"/>
+                    <rect x="1.5" y="1.5" width="13.014" height="13.014" fill="none"/>
+                    </g>
+                    <g id="Rectangle_69" data-name="Rectangle 69" transform="translate(70.794 69.34)" fill="none" stroke="#34342c" strokeWidth="3">
+                    <rect width="16.014" height="16.014" stroke="none"/>
+                    <rect x="1.5" y="1.5" width="13.014" height="13.014" fill="none"/>
+                    </g>
+                    <g id="Rectangle_71" data-name="Rectangle 71" transform="translate(13.41 69.34)" fill="none" stroke="#34342c" strokeWidth="3">
+                    <rect width="16.014" height="16.014" stroke="none"/>
+                    <rect x="1.5" y="1.5" width="13.014" height="13.014" fill="none"/>
+                    </g>
+                    <g id="maze" transform="translate(0 38.971)">
+                    <path id="Path_49" data-name="Path 49" d="M23.427,79.754H54.149v8.777" transform="translate(-23.427 -69.44)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    <path id="Path_50" data-name="Path 50" d="M31.427,82.576V73.725" transform="translate(-19.682 -73.725)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    <path id="Path_51" data-name="Path 51" d="M39.2,82.512V73.861" transform="translate(-16.045 -73.661)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    </g>
+                    <g id="maze-2" data-name="maze" transform="translate(59.832 0) rotate(90)">
+                    <path id="Path_49-2" data-name="Path 49" d="M0,0H30.722V8.777" transform="translate(0 10.113)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    <path id="Path_51-2" data-name="Path 51" d="M0,8.651V0" transform="translate(23.15)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    <path id="Path_52" data-name="Path 52" d="M0,0V8.777" transform="translate(10.632 10.113)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    </g>
+                    <g id="maze-3" data-name="maze" transform="translate(68.437 39.172)">
+                    <path id="Path_49-3" data-name="Path 49" d="M56.5,79.754H28.152" transform="translate(-28.152 -69.641)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    <path id="Path_50-2" data-name="Path 50" d="M31.427,82.576V73.725" transform="translate(-13.631 -73.725)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    <path id="Path_51-3" data-name="Path 51" d="M39.2,85.867V73.861" transform="translate(-37.436 -73.861)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    <path id="Path_52-2" data-name="Path 52" d="M38.668,79.754v8.777" transform="translate(-29.87 -69.641)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    </g>
+                    <g id="maze-4" data-name="maze" transform="translate(39.525 97.644) rotate(-90)">
+                    <path id="Path_49-4" data-name="Path 49" d="M0,0H30.722V8.777" transform="translate(0 10.314)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    <path id="Path_50-3" data-name="Path 50" d="M0,8.852V0" transform="translate(13.213)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    <path id="Path_51-4" data-name="Path 51" d="M0,8.651V0" transform="translate(40.688 10.314) rotate(90)" fill="none" stroke="#34342c" strokeWidth="3"/>
+                    </g>
+                </g>
+                <g id="mobileinhand" transform="translate(123.989 46.145)">
+                    <g id="Group_38" data-name="Group 38" transform="translate(9.351 92.082)">
+                    <path id="Path_54" data-name="Path 54" d="M98,109.145v5.861l25.913,23.819v15.467" transform="translate(-97.998 -108.736)" fill="none" stroke="#fff" strokeWidth="3"/>
+                    <g id="Rectangle_75" data-name="Rectangle 75" transform="translate(14.67 0)" fill="#fff" stroke="#fff" strokeLinejoin="round" strokeWidth="1">
+                        <rect width="4.004" height="4.004" stroke="none"/>
+                        <rect x="0.5" y="0.5" width="3.004" height="3.004" fill="none"/>
+                    </g>
+                    <g id="Rectangle_76" data-name="Rectangle 76" transform="translate(28.016 0)" fill="#fff" stroke="#fff" strokeLinejoin="round" strokeWidth="1">
+                        <rect width="4.004" height="4.004" stroke="none"/>
+                        <rect x="0.5" y="0.5" width="3.004" height="3.004" fill="none"/>
+                    </g>
+                    <g id="Rectangle_77" data-name="Rectangle 77" transform="translate(37.357 0)" fill="#fff" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1">
+                        <rect width="5.338" height="4.004" stroke="none"/>
+                        <rect x="0.5" y="0.5" width="4.338" height="3.004" fill="none"/>
+                    </g>
+                    </g>
+                    <g id="Group_39" data-name="Group 39" transform="translate(6.673 0)">
+                    <g id="Rectangle_73" data-name="Rectangle 73" transform="translate(0)" fill="#683c45" stroke="#fff" strokeWidth="3">
+                        <rect width="61.388" height="106.761" rx="5" stroke="none"/>
+                        <rect x="1.5" y="1.5" width="58.388" height="103.761" rx="3.5" fill="none"/>
+                    </g>
+                    <g id="Rectangle_74" data-name="Rectangle 74" transform="translate(13.345)" fill="none" stroke="#fff" strokeWidth="3">
+                        <path d="M0,0H34.7a0,0,0,0,1,0,0V12.349a5,5,0,0,1-5,5H5a5,5,0,0,1-5-5V0A0,0,0,0,1,0,0Z" stroke="none"/>
+                        <path d="M1.5,1.5H33.2a0,0,0,0,1,0,0V12.349a3.5,3.5,0,0,1-3.5,3.5H5a3.5,3.5,0,0,1-3.5-3.5V1.5A0,0,0,0,1,1.5,1.5Z" fill="none"/>
+                    </g>
+                    <path id="Path_53" data-name="Path 53" d="M107.845,74.89l10.814,9.425,17.973-19.034" transform="translate(-90.711 -37.186)" fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="5"/>
+                    </g>
+                    <g id="Rectangle_78" data-name="Rectangle 78" transform="translate(0 48.043)" fill="#fff" stroke="#34342c" strokeWidth="2">
+                    <rect width="21.352" height="12.011" rx="5" stroke="none"/>
+                    <rect x="1" y="1" width="19.352" height="10.011" rx="4" fill="none"/>
+                    </g>
+                    <g id="Rectangle_79" data-name="Rectangle 79" transform="translate(0 57.384)" fill="#fff" stroke="#34342c" strokeWidth="2">
+                    <rect width="21.352" height="13.345" rx="5" stroke="none"/>
+                    <rect x="1" y="1" width="19.352" height="11.345" rx="4" fill="none"/>
+                    </g>
+                    <g id="Rectangle_80" data-name="Rectangle 80" transform="translate(0 69.395)" fill="#fff" stroke="#34342c" strokeWidth="2">
+                    <rect width="21.352" height="13.345" rx="5" stroke="none"/>
+                    <rect x="1" y="1" width="19.352" height="11.345" rx="4" fill="none"/>
+                    </g>
+                    <g id="Rectangle_81" data-name="Rectangle 81" transform="translate(0 81.405)" fill="#fff" stroke="#34342c" strokeWidth="2">
+                    <rect width="21.352" height="12.011" rx="5" stroke="none"/>
+                    <rect x="1" y="1" width="19.352" height="10.011" rx="4" fill="none"/>
+                    </g>
+                    <path id="Path_55" data-name="Path 55" d="M136.075,94.507l5.536-9.15s3.943-2.132,5.071-1.333,1.446,4.627,1.446,4.627v74" transform="translate(-69.616 -26.381)" fill="none" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3"/>
+                </g>
+                </g>
+                {/* <g id="number-icon" transform="translate(-97.6 -19.349)">
+                <g id="Rectangle_62" data-name="Rectangle 62" transform="translate(0 0.495)" fill="#683c45" stroke="#683c45" strokeWidth="1">
+                    <rect width="45" height="45" rx="14" stroke="none"/>
+                    <rect x="0.5" y="0.5" width="44" height="44" rx="13.5" fill="none"/>
+                </g>
+                <text id="_2" data-name="2" transform="translate(14.346 29.558)" fill="#fff" fontSize="24" fontFamily="SpaceGrotesk-Bold, Space Grotesk" fontWeight="700"><tspan x="0" y="0">2</tspan></text>
+                </g> */}
+            </g>
+        </svg>
+    </div>
+)};
